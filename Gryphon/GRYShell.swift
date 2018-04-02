9c92d0482eb0d82b52914bfee3a6d0471ff17ecc
Vini <vinivendra@gmail.com>
2018-04-02 18:54:47 -0300

    Read input files automatically
    Now the program only needs the input file path. It calls the swift compiler with the appropriate flags, it cleans the output from the compiler and parses it automatically.

---
+++ Gryphon/GRYShell.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+internal enum Shell {
+	@discardableResult
+	static func runShellCommand(_ command: String) -> (output: String, error: String, status: Int32) {
+		let arguments = command.components(separatedBy: " ")
+		let outputPipe = Pipe()
+		let errorPipe = Pipe()
+		let task = Process()
+		task.launchPath = "/usr/bin/env"
+		task.arguments = arguments
+		task.standardOutput = outputPipe
+		task.standardError = errorPipe
+		task.launch()
+		task.waitUntilExit()
+		
+		let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+		let outputString = String(data: outputData, encoding: .utf8) ?? ""
+		
+		let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+		let errorString = String(data: errorData, encoding: .utf8) ?? ""
+		
+		return (output: outputString,
+				error: errorString,
+				status: task.terminationStatus)
+	}
+	
+	static func writeString(_ contents: String, toFileAtPath filePath: String) {
+		if FileManager.default.fileExists(atPath: filePath) {
+			try? FileManager.default.removeItem(atPath: filePath)
+		}
+		
+		let success = FileManager.default.createFile(
+			atPath: filePath,
+			contents: nil)
+		guard success else { return }
+		
+		guard let data = contents.data(using: .utf8),
+			let file = FileHandle(forUpdatingAtPath: filePath)
+			else { return }
+		
+		file.seek(toFileOffset: 0)
+		file.write(data)
+		file.closeFile()
+	}
+}
--- Gryphon/main.swift
+++ Gryphon/main.swift
@@ -1,36 +1,15 @@
 // TODO: Automatically calculate -dump-ast from terminal output
 // TODO: Move test.swift into the project directory, change absolute paths to be relative, change the project's location
 
-import Foundation
-
-var contents = """
-(source_file
-  (func_decl "foo(bar:baz:)" interface type='(Int, String) -> ()' access=internal
-    (parameter_list
-      (parameter "bar" apiName=bar type='Int' interface type='Int')
-      (parameter "baz" apiName=baz type='String' interface type='String'))
-    (brace_stmt
-      (pattern_binding_decl
-        (pattern_typed type='Int'
-          (pattern_named type='Int' 'x')
-          (type_ident
-            (component id='Int' bind=Swift.(file).Int)))
-        (call_expr implicit type='Int' location=test.swift:3:15 range=[test.swift:3:15 - line:3:15] nothrow arg_labels=_builtinIntegerLiteral:
-          (constructor_ref_call_expr implicit type='(_MaxBuiltinIntegerType) -> Int' location=test.swift:3:15 range=[test.swift:3:15 - line:3:15] nothrow
-            (declref_expr implicit type='(Int.Type) -> (_MaxBuiltinIntegerType) -> Int' location=test.swift:3:15 range=[test.swift:3:15 - line:3:15] decl=Swift.(file).Int.init(_builtinIntegerLiteral:) function_ref=single)
-            (type_expr implicit type='Int.Type' location=test.swift:3:15 range=[test.swift:3:15 - line:3:15] typerepr='Int'))
-          (tuple_expr implicit type='(_builtinIntegerLiteral: Int2048)' location=test.swift:3:15 range=[test.swift:3:15 - line:3:15] names=_builtinIntegerLiteral
-            (integer_literal_expr type='Int2048' location=test.swift:3:15 range=[test.swift:3:15 - line:3:15] value=0))))
-
-      (var_decl "x" type='Int' interface type='Int' access=private storage_kind=stored)
-)))
-"""
-
-////////////////////////////////////////////////////////////////////////////////
-
 func main() {
+	let filePath = "/Users/vini/Desktop/test.swift"
+	let command = "swiftc -dump-ast \(filePath)"
+	let commandResult = Shell.runShellCommand(command)
+	var commandOutput = commandResult.error
+	commandOutput =~ "^((.*)\n)*\\(source\\_file\n" => "\\(source\\_file\n"
+	
 	print("-- Gryphon AST --")
-	let ast = GRYAst(fileContents: contents)
+	let ast = GRYAst(fileContents: commandOutput)
 	ast.prettyPrint()
 	
 	print()
