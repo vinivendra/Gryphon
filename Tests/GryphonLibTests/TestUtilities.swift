4401947c96b3dd6b6e7ecdf0b7f8b192c869ae2b
Vinicius Vendramini <vinivendra@me.com>
2018-04-12 14:18:28 -0300

    Added a method to seed from the last hour instead of the current hour.
    If a bug is found with the random number generator and the hour passes, this method allows easy changing of the seed for repeating the bug for another hour. All that's needed is to change the `rng` variable (immediately below the method) to seed with the `seedFromLastHour` method.
    
    If another hour passes, or if the user wants to record that specific seed, the method is easy to understand and change accordingly. All used numbers have very intuitive values. Specifically, weekdays are counted starting in Sunday (meaning Thursday is day `5`) and hours are counted in 24h style (so 2PM is `14`).

--- Tests/GryphonLibTests/TestUtilities.swift
+++ Tests/GryphonLibTests/TestUtilities.swift
@@ -17,6 +17,22 @@ func seedFromCurrentHour() -> (UInt64, UInt64) {
 	return (UInt64(int1), UInt64(int2))
 }
 
+func seedFromLastHour() -> (UInt64, UInt64) {
+	let calendar = Calendar.current
+	let now = Date()
+	let year = calendar.component(.year, from: now)
+	let month = calendar.component(.month, from: now)
+	let week = calendar.component(.weekOfYear, from: now)
+	let weekday = calendar.component(.weekday, from: now)
+	let day = calendar.component(.day, from: now)
+	let lastHour = (calendar.component(.hour, from: now) + 23) % 24
+	
+	let int1 = year + month + week
+	let int2 = weekday + day + lastHour
+	
+	return (UInt64(int1), UInt64(int2))
+}
+
 enum TestUtils {
 	static var rng: RandomGenerator = Xoroshiro(seed: seedFromCurrentHour())
 }
