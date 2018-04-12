107d6be68634217539eb7d4f79fb1ce04639c31d
Vinicius Vendramini <vinivendra@me.com>
2018-04-12 14:19:14 -0300

    - Changed variable name for readability.

--- Tests/GryphonLibTests/TestUtilities.swift
+++ Tests/GryphonLibTests/TestUtilities.swift
@@ -6,12 +6,12 @@ func seedFromCurrentHour() -> (UInt64, UInt64) {
 	let now = Date()
 	let year = calendar.component(.year, from: now)
 	let month = calendar.component(.month, from: now)
-	let week = calendar.component(.weekOfYear, from: now)
+	let weekOfYear = calendar.component(.weekOfYear, from: now)
 	let weekday = calendar.component(.weekday, from: now)
 	let day = calendar.component(.day, from: now)
 	let hour = calendar.component(.hour, from: now)
 	
-	let int1 = year + month + week
+	let int1 = year + month + weekOfYear
 	let int2 = weekday + day + hour
 	
 	return (UInt64(int1), UInt64(int2))
@@ -22,12 +22,12 @@ func seedFromLastHour() -> (UInt64, UInt64) {
 	let now = Date()
 	let year = calendar.component(.year, from: now)
 	let month = calendar.component(.month, from: now)
-	let week = calendar.component(.weekOfYear, from: now)
+	let weekOfYear = calendar.component(.weekOfYear, from: now)
 	let weekday = calendar.component(.weekday, from: now)
 	let day = calendar.component(.day, from: now)
 	let lastHour = (calendar.component(.hour, from: now) + 23) % 24
 	
-	let int1 = year + month + week
+	let int1 = year + month + weekOfYear
 	let int2 = weekday + day + lastHour
 	
 	return (UInt64(int1), UInt64(int2))
