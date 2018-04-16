bcffc28e1b0f522ff5bec1ac4029062a8550f6e7
Vinicius Vendramini <vinivendra@me.com>
2018-04-16 18:09:58 -0300

    Modularized the compiler.
    This creates a better interface for the user and will enable better tests (coming in the next commits).

--- Sources/GryphonLib/GRYCompiler.swift
+++ Sources/GryphonLib/GRYCompiler.swift
@@ -6,35 +6,44 @@ import Darwin
 
 public enum GRYCompiler {
     public static func compile(fileAt filePath: String) -> String {
-        // Call the swift compiler
-        let command = ["swiftc", "-dump-ast", filePath]
-        let commandResult = Shell.runShellCommand(command)
-        
-        // Ensure the compiler terminated successfully
-        guard commandResult.status == 0 else {
-            print("Error parsing and typechecking input files. Swift compiler says:\n\(commandResult.standardError)")
-            exit(commandResult.status)
-        }
-        
-        // The compiler has dumped the ast to stderr
-        var astDump = commandResult.standardError
-        
-        print(astDump)
-        
-        // Trim any warnings swift may have printed before the actual AST dump
-        astDump =~ "^((.*)\n)*\\(source\\_file\n" => "\\(source\\_file\n"
-        
-        // Parse the AST into Gryphon data structures
-        print("-- Gryphon AST --")
-        let ast = GRYAst(fileContents: astDump)
-        ast.prettyPrint()
-        print()
-        
-        // Translate the AST to Kotlin
+        let ast = generateAST(forFileAt: filePath)
+		
+		// Translate the AST to Kotlin
         print("-- Kotlin --")
         let kotlin = GRYKotlinTranslator().translateAST(ast)
         print(kotlin)
         
         return kotlin
     }
+
+	public static func generateAST(forFileAt filePath: String) -> GRYAst {
+		let astDump = getSwiftASTDump(forFileAt: filePath)
+		
+		// Parse the AST into Gryphon data structures
+		print("-- Gryphon AST --")
+		let ast = GRYAst(fileContents: astDump)
+		ast.prettyPrint()
+		
+		return ast
+	}
+	
+	public static func getSwiftASTDump(forFileAt filePath: String) -> String {
+		// Call the swift compiler
+		let command = ["swiftc", "-dump-ast", filePath]
+		let commandResult = Shell.runShellCommand(command)
+		
+		// Ensure the compiler terminated successfully
+		guard commandResult.status == 0 else {
+			print("Error parsing and typechecking input files. Swift compiler says:\n\(commandResult.standardError)")
+			exit(commandResult.status)
+		}
+		
+		// The compiler has dumped the ast to stderr
+		var astDump = commandResult.standardError
+
+		// Trim any warnings swift may have printed before the actual AST dump
+		astDump =~ "^((.*)\n)*\\(source\\_file\n" => "\\(source\\_file\n"
+		
+		return astDump
+	}
 }
