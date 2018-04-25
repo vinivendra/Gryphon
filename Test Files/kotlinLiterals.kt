2b586ad10f621ee6255bddca4be6ba94124a6e4c
Vinicius Vendramini <vinivendra@me.com>
2018-04-25 16:46:20 -0300

    Added more tests for GRYKotlinLiteral's and GRYKotlinIgnoreNext.

--- Test Files/kotlinLiterals.kt
+++ Test Files/kotlinLiterals.kt
@@ -1,6 +1,9 @@
-
 fun main(args: Array<String>) {
 	val languageName: String = "kotlin"
 	println("Hello from ${(languageName)}!")
+	val magicNumber: Int = 40 + 5-3
+	println(magicNumber)
 	println("This will be ignored by swift, but not by kotlin.")
+	val squareRoot: Double = Math.sqrt(9.0)
+	println(squareRoot)
 }
--- Test Files/kotlinLiterals.swift
+++ Test Files/kotlinLiterals.swift
@@ -1,9 +1,28 @@
+func GRYKotlinIgnoreNext() { }
+
 func GRYKotlinLiteral(_ kotlinExpression: String) { }
 
 func GRYKotlinLiteral<T>(_ swiftExpression: T, _ kotlinExpression: String) -> T {
 	return swiftExpression
 }
 
+// Kotlin literals as expressions
 let languageName = GRYKotlinLiteral("swift", "\"kotlin\"")
 print("Hello from \(languageName)!")
+
+let magicNumber = 40 + GRYKotlinLiteral(2, "5-3")
+print(magicNumber)
+
+// Kotlin literals as statements
 GRYKotlinLiteral("println(\"This will be ignored by swift, but not by kotlin.\")")
+
+// Ignore swift statements
+GRYKotlinIgnoreNext()
+import Foundation // There's no Foundation in kotlin
+
+GRYKotlinIgnoreNext()
+print("This will be ignored by kotlin, but not by swift.")
+
+// Call something swift can't parse
+let squareRoot = GRYKotlinLiteral(sqrt(9), "Math.sqrt(9.0)")
+print(squareRoot)
