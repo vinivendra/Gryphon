283f1b2abfda507f4fdb133c1831f830fb1a8f7a
Vinicius Vendramini <vinivendra@gmail.com>
2019-03-10 20:44:37 -0300

    Test - Fix tests to match the new empty-struct style

--- Test Files/structs.kt
+++ Test Files/structs.kt
@@ -1,17 +1,17 @@
 data class SupportedStruct(
 	val x: Int = 0,
 	val y: Int = 1
-) { }
+)
 
 data class OtherSupportedStruct(
 	val x: Int,
 	val y: Int
-) { }
+)
 
 data class NoInheritance(
 	val x: Int,
 	val y: Int
-) { }
+)
 
 fun main(args: Array<String>) {
 	val a: SupportedStruct = SupportedStruct()
