ddd937c761b493bb29819e34d669f8c980db712b
Vinicius Vendramini <vinivendra@gmail.com>
2019-03-19 15:54:36 -0300

    Test - update arrays test to use new ArrayReference implementation

--- Test Files/arrays.swift
+++ Test Files/arrays.swift
@@ -14,31 +14,39 @@
 //* limitations under the License.
 //*/
 
-/// According to http://swiftdoc.org/v3.0/type/Array/hierarchy/
+/// According to http://swiftdoc.org/v4.2/type/Array/hierarchy/
 /// (link found via https://www.raywenderlich.com/139591/building-custom-collection-swift)
 /// the Array type in Swift conforms exactly to these protocols,
 /// plus CustomReflectable (which is beyond Gryphon's scope for now).
-
-class ArrayReference<Element>: // kotlin: ignore
+public final class ArrayReference<Element>: // kotlin: ignore
 	ExpressibleByArrayLiteral, CustomStringConvertible, CustomDebugStringConvertible,
 	RandomAccessCollection, MutableCollection, RangeReplaceableCollection
 {
-	typealias ArrayLiteralElement = Element
+	public typealias Buffer = [Element]
 
-	//
-	var array: Array<Element>
+	public var array: Buffer
 
-	init(array: Array<Element>) {
+	public init(array: Buffer) {
 		self.array = array
 	}
 
+	public init<T>(_ arrayReference: ArrayReference<T>) {
+		self.array = arrayReference.array as! Buffer
+	}
+
+	public func copy() -> ArrayReference<Element> {
+		return ArrayReference(array: array)
+	}
+
 	// Expressible By Array Literal
-	required init(arrayLiteral elements: Element...) {
+	public typealias ArrayLiteralElement = Element
+
+	public required init(arrayLiteral elements: Element...) {
 		self.array = elements
 	}
 
 	// ...
-	subscript (_ index: Int) -> Element {
+	public subscript (_ index: Int) -> Element {
 		get {
 			return array[index]
 		}
@@ -48,43 +56,92 @@ class ArrayReference<Element>: // kotlin: ignore
 	}
 
 	// Custom (Debug) String Convertible
-	var description: String {
+	public var description: String {
 		return array.description
 	}
 
-	var debugDescription: String {
+	public var debugDescription: String {
 		return array.debugDescription
 	}
 
 	// Collection
-	var startIndex: Int {
+	public var startIndex: Int {
 		return array.startIndex
 	}
 
-	var endIndex: Int {
+	public var endIndex: Int {
 		return array.endIndex
 	}
 
-	func index(after i: Int) -> Int {
+	public func index(after i: Int) -> Int {
 		return i + 1
 	}
 
 	// Bidirectional Collection
-	func index(before i: Int) -> Int {
+	public func index(before i: Int) -> Int {
 		return i - 1
 	}
 
 	// Range Replaceable Collection
-	func append<S>(contentsOf newElements: S) where S : Sequence, Element == S.Element {
+	public func append<S>(contentsOf newElements: S) where S: Sequence, Element == S.Element {
 		self.array.append(contentsOf: newElements)
 	}
 
-	required init<S>(_ elements: S) where S : Sequence, Element == S.Element {
-		self.array = Array<Element>(elements)
+	public required init<S>(_ elements: S) where S: Sequence, Element == S.Element {
+		self.array = []
 	}
 
-	required init() {
-		self.array = Array<Element>()
+	public required init() {
+		self.array = []
+	}
+
+	//
+	public func append(_ newElement: Element) {
+		array.append(newElement)
+	}
+
+	public func appending(_ newElement: Element) -> ArrayReference<Element> {
+		return ArrayReference<Element>(array: self.array + [newElement])
+	}
+
+	public func filter(_ isIncluded: (Element) throws -> Bool) rethrows -> ArrayReference<Element> {
+		return try ArrayReference(array: self.array.filter(isIncluded))
+	}
+
+	public func map<T>(_ transform: (Element) throws -> T) rethrows -> ArrayReference<T> {
+		return try ArrayReference<T>(array: self.array.map(transform))
+	}
+
+	public func appending<S>(contentsOf newElements: S) -> ArrayReference<Element>
+		where S: Sequence, Element == S.Element
+	{
+		return ArrayReference<Element>(array: self.array + newElements)
+	}
+
+	public func removeFirst() -> Element {
+		return array.removeFirst()
+	}
+}
+
+extension ArrayReference: Equatable where Element: Equatable { // kotlin: ignore
+	public static func == (lhs: ArrayReference, rhs: ArrayReference) -> Bool {
+		return lhs.array == rhs.array
+	}
+}
+
+extension ArrayReference: Hashable where Element: Hashable { // kotlin: ignore
+	public func hash(into hasher: inout Hasher) {
+		array.hash(into: &hasher)
+	}
+}
+
+extension ArrayReference: Codable where Element: Codable { // kotlin: ignore
+	public func encode(to encoder: Encoder) throws {
+		try array.encode(to: encoder)
+	}
+
+	public convenience init(from decoder: Decoder) throws {
+		try self.init(array: Buffer(from: decoder))
 	}
 }
 
