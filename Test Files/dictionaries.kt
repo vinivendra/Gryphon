8b0ca27722b7f38209e64bdf5968ab05dd5c45c0
Vinicius Vendramini <vinivendra@gmail.com>
2019-03-19 15:54:47 -0300

    Test - Add dictionary tests

--- Test Files/dictionaries.kt
+++ Test Files/dictionaries.kt
@@ -1,10 +1,29 @@
 fun main(args: Array<String>) {
+	val dictionaryA: MutableMap<String, Int> = mutableMapOf("a" to 1, "b" to 2, "c" to 3)
+	val dictionaryB: MutableMap<Int, Int> = mutableMapOf()
+
+	println(dictionaryA["a"])
+	println(dictionaryA["b"])
+	println(dictionaryA["c"])
+	println(dictionaryA["d"])
+	println(dictionaryB[0])
+
 	val dictionary1: MutableMap<String, Int> = mutableMapOf("a" to 1, "b" to 2, "c" to 3)
-	val dictionary2: MutableMap<Int, Int> = mutableMapOf()
+	val dictionary2: MutableMap<String, Int> = dictionary1
+
+	dictionary1["a"] = 10
 
 	println(dictionary1["a"])
 	println(dictionary1["b"])
 	println(dictionary1["c"])
 	println(dictionary1["d"])
-	println(dictionary2[0])
+	println(dictionary2["a"])
+	println(dictionary2["b"])
+	println(dictionary2["c"])
+	println(dictionary2["d"])
+
+	val dictionary3: MutableMap<String, Int> = mutableMapOf()
+
+	println(dictionary3["a"])
+	println(dictionary3["d"])
 }
--- Test Files/dictionaries.swift
+++ Test Files/dictionaries.swift
@@ -14,11 +14,146 @@
 //* limitations under the License.
 //*/
 
-let dictionary1 = ["a": 1, "b": 2, "c": 3]
-let dictionary2: [Int: Int] = [:]
+/// According to https://swiftdoc.org/v4.2/type/dictionary/hierarchy/
+/// the Dictionary type in Swift conforms exactly to these protocols,
+/// plus CustomReflectable (which is beyond Gryphon's scope for now).
+public final class DictionaryReference<Key, Value>: // kotlin: ignore
+	ExpressibleByDictionaryLiteral, CustomStringConvertible, CustomDebugStringConvertible,
+	Collection
+	where Key: Hashable
+{
+	public typealias Buffer = [Key: Value]
+
+	public var dictionary: Buffer
+
+	public init(dictionary: Buffer) {
+		self.dictionary = dictionary
+	}
+
+	public init<K, V>(_ dictionaryReference: DictionaryReference<K, V>) {
+		self.dictionary = dictionaryReference.dictionary as! Buffer
+	}
+
+	public func copy() -> DictionaryReference<Key, Value> {
+		return DictionaryReference(dictionary: dictionary)
+	}
+
+	// Expressible By Dictionary Literal
+	public required init(dictionaryLiteral elements: (Key, Value)...) {
+		self.dictionary = Buffer(uniqueKeysWithValues: elements)
+	}
+
+	// ...
+	public subscript (_ key: Key) -> Value? {
+		get {
+			return dictionary[key]
+		}
+		set {
+			dictionary[key] = newValue
+		}
+	}
+
+	// Custom (Debug) String Convertible
+	public var description: String {
+		return dictionary.description
+	}
+
+	public var debugDescription: String {
+		return dictionary.debugDescription
+	}
+
+	// Collection
+	public typealias SubSequence = Slice<[Key: Value]>
+
+	@inlinable public var startIndex: Buffer.Index {
+		return dictionary.startIndex
+	}
+
+	@inlinable public var endIndex: Buffer.Index {
+		return dictionary.endIndex
+	}
+
+	@inlinable
+	public func index(after i: Buffer.Index) -> Buffer.Index
+	{
+		return dictionary.index(after: i)
+	}
+
+	@inlinable
+	public func formIndex(after i: inout Buffer.Index) {
+		dictionary.formIndex(after: &i)
+	}
+
+	@inlinable
+	public func index(forKey key: Key) -> Buffer.Index? {
+		return dictionary.index(forKey: key)
+	}
+
+	@inlinable
+	public subscript(position: Buffer.Index) -> Buffer.Element {
+		return dictionary[position]
+	}
+
+	@inlinable public var count: Int {
+		return dictionary.count
+	}
+
+	@inlinable public var isEmpty: Bool {
+		return dictionary.isEmpty
+	}
+}
+
+extension DictionaryReference: Equatable where Value: Equatable { // kotlin: ignore
+	public static func == (
+		lhs: DictionaryReference, rhs: DictionaryReference) -> Bool
+	{
+		return lhs.dictionary == rhs.dictionary
+	}
+}
+
+extension DictionaryReference: Hashable where Value: Hashable { // kotlin: ignore
+	public func hash(into hasher: inout Hasher) {
+		dictionary.hash(into: &hasher)
+	}
+}
+
+extension DictionaryReference: Codable where Key: Codable, Value: Codable { // kotlin: ignore
+	public func encode(to encoder: Encoder) throws {
+		try dictionary.encode(to: encoder)
+	}
+
+	public convenience init(from decoder: Decoder) throws {
+		try self.init(dictionary: Buffer(from: decoder))
+	}
+}
+
+// Traditional dictionaries
+let dictionaryA = ["a": 1, "b": 2, "c": 3]
+let dictionaryB: [Int: Int] = [:]
+
+print(dictionaryA["a"])
+print(dictionaryA["b"])
+print(dictionaryA["c"])
+print(dictionaryA["d"])
+
+print(dictionaryB[0])
+
+// Dictionary references
+let dictionary1: DictionaryReference = ["a": 1, "b": 2, "c": 3]
+let dictionary2 = dictionary1
+dictionary1["a"] = 10
 
 print(dictionary1["a"])
 print(dictionary1["b"])
 print(dictionary1["c"])
 print(dictionary1["d"])
-print(dictionary2[0])
+
+print(dictionary2["a"])
+print(dictionary2["b"])
+print(dictionary2["c"])
+print(dictionary2["d"])
+
+let dictionary3: DictionaryReference<String, Int> = [:]
+
+print(dictionary3["a"])
+print(dictionary3["d"])
