0284992fcd37f7627bc3ea8cf2f5ab5ff637ca2a
Vinicius Vendramini <vinivendra@gmail.com>
2019-12-14 16:07:18 -0300

    Development: improve preBuildScript to make sure it exits when it fails

--- preBuildScript.sh
+++ preBuildScript.sh
@@ -1,10 +1,18 @@
 # Update AST dumps
 echo " ➡️  Updating AST dumps for tests and library templates..."
+
 perl dumpASTs.pl ".gryphon/StandardLibrary.template.swift"
+[ $? -eq 0 ] || exit 1
+
 perl dumpASTs.pl ".gryphon/GryphonXCTest.swift"
+[ $? -eq 0 ] || exit 1
+
 perl dumpASTs.pl "Example ASTs/test.swift"
+[ $? -eq 0 ] || exit 1
+
 for testFile in Test\ Files/*.swift; do
     perl dumpASTs.pl "$testFile"
+    [ $? -eq 0 ] || exit 1
 done
 
 # Lint swift files
