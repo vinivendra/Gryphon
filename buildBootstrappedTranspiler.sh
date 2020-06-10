eba49c375803688b19e348da38fc274b71fdb513
Antoine Barrault <barrault01@gmail.com>
2020-06-10 18:22:06 -0300

    adding chmod 700 to every sh

--- buildBootstrappedTranspiler.sh
+++ buildBootstrappedTranspiler.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 kotlinc -include-runtime \
 	-d Bootstrap/kotlin.jar \
 	Bootstrap/*.kt
--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 swift build && cp .build/debug/Gryphon /usr/local/bin/gryphon
 
