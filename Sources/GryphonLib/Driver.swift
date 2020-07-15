da0eb1afaa68df580c5404262eb0d57c370b8640
Vini Vendramini <vinivendra@me.com>
2020-07-15 18:52:26 -0300

    Meta: version bump to 0.10.1

--- Sources/GryphonLib/Driver.swift
+++ Sources/GryphonLib/Driver.swift
@@ -22,7 +22,7 @@
 // gryphon output: Bootstrap/Driver.kt
 
 public class Driver {
-	public static let gryphonVersion = "0.10"
+	public static let gryphonVersion = "0.10.1"
 
 	public static let supportedArguments: List = [
 		"help", "-help", "--help",
