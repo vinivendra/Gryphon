a917b7f3028a7294096ecf7c43264e1d7f4b2ff4
Vini Vendramini <vinivendra@me.com>
2020-10-27 15:28:55 -0300

    Revert accidental changes to test.swift

--- test.swift
+++ test.swift
@@ -1,21 +1,23 @@
-//var x: Int = 0
-//var y = x
-//var z = 10
-//z = x
 //
-//print("\(x)")
-//print("\(y)")
-//print("\(z)")
+// Copyright 2018 Vinicius Jorge Vendramini
 //
-//let a: Int? = nil
+// Licensed under the Hippocratic License, Version 2.1;
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//print("\(a)")
+// https://firstdonoharm.dev/version/2/1/license.md
 //
-////
-//let i: Int
-//i = 0
+// To the full extent allowed by law, this software comes "AS IS,"
+// WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED, and licensor and any other
+// contributor shall not be liable to anyone for any damages or other
+// liability arising from, out of, or in connection with the sotfware
+// or this license, under any kind of legal claim.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
-     let r, g, b: Double
-//r = 10
-//g = 10
-//b = 10
+
+let a: Any = ""
+
+if let a = a as? String {
+	print("It's a string!")
+}
