fc1da867ed541bdb189a35e47148287aebb04e67
Vini Vendramini <vinivendra@me.com>
2021-05-23 13:30:57 -0300

    Cleanup

--- Sources/GryphonLib/SwiftType.swift
+++ Sources/GryphonLib/SwiftType.swift
@@ -242,6 +242,7 @@ extension SwiftType {
 			return nil
 		}
 	}
+
 	var asOptional: Optional? {
 		switch self.contents {
 		case let .optional(refinedType):
@@ -250,6 +251,7 @@ extension SwiftType {
 			return nil
 		}
 	}
+
 	var asDot: Dot? {
 		switch self.contents {
 		case let .dot(refinedType):
@@ -258,6 +260,7 @@ extension SwiftType {
 			return nil
 		}
 	}
+
 	var asTuple: Tuple? {
 		switch self.contents {
 		case let .tuple(refinedType):
@@ -266,6 +269,7 @@ extension SwiftType {
 			return nil
 		}
 	}
+
 	var asFunction: Function? {
 		switch self.contents {
 		case let .function(refinedType):
@@ -274,6 +278,7 @@ extension SwiftType {
 			return nil
 		}
 	}
+
 	var asGeneric: Generic? {
 		switch self.contents {
 		case let .generic(refinedType):
@@ -298,6 +303,7 @@ extension SwiftType {
 			return SwiftType(.named(self))
 		}
 	}
+
 	struct Optional: SwiftTypeRefined, Equatable {
 		let subType: SwiftType
 
@@ -314,6 +320,7 @@ extension SwiftType {
 			return SwiftType(.optional(self))
 		}
 	}
+
 	struct Dot: SwiftTypeRefined, Equatable {
 		let leftType: SwiftType
 		let rightType: String
@@ -326,6 +333,7 @@ extension SwiftType {
 			return SwiftType(.dot(self))
 		}
 	}
+
 	struct Tuple: SwiftTypeRefined, Equatable {
 		let subTypes: MutableList<LabeledSwiftType>
 
@@ -338,6 +346,7 @@ extension SwiftType {
 			return SwiftType(.tuple(self))
 		}
 	}
+
 	struct Function: SwiftTypeRefined, Equatable {
 		let parameters: MutableList<SwiftType>
 		let returnType: SwiftType
@@ -351,6 +360,7 @@ extension SwiftType {
 			return SwiftType(.function(self))
 		}
 	}
+	
 	struct Generic: SwiftTypeRefined, Equatable {
 		let typeName: String
 		let genericArguments: MutableList<SwiftType>
@@ -384,440 +394,3 @@ struct LabeledSwiftType: CustomStringConvertible, Equatable {
 		}
 	}
 }
-
-//	static func create(fromString string: String) -> SwiftType? {
-//		return Parser(string: string).parse()
-//	}
-//
-//	private class Parser {
-//		let string: String
-//		var index: String.Index
-//
-//		init(string: String) {
-//			self.string = string
-//			self.index = string.startIndex
-//		}
-//
-//		func parse() -> SwiftType? {
-//			guard !string.isEmpty else {
-//				return nil
-//			}
-//
-//			let result = parseType()
-//			cleanLeadingWhitespace()
-//
-//			// If there's still something left to parse, then something went wrong.
-//			guard index == string.endIndex else {
-//				return nil
-//			}
-//
-//			return result
-//		}
-//
-//		func cleanLeadingWhitespace() {
-//			while index != string.endIndex, string[index] == " " {
-//				index = string.index(after: index)
-//			}
-//		}
-//
-//		/// Parses the next type, starting at `index`. Leaves `index` in the position after the end
-//		/// of the parsed type if successful; returns `nil` otherwise.
-//		private func parseType() -> SwiftType? {
-//			guard let nonOptionalType = parseNonOptionalType() else {
-//				return nil
-//			}
-//
-//			var result = nonOptionalType
-//
-//			/// Checks for "?"s after the type
-//			while index != string.endIndex, string[index] == "?" {
-//				index = string.index(after: index)
-//				result = .optional(subType: result)
-//			}
-//
-//			return result
-//		}
-//
-//		/// Parses a type ignoring possible "?"s at its end
-//		private func parseNonOptionalType() -> SwiftType? {
-//			cleanLeadingWhitespace()
-//
-//			// TODO:
-//			// - autoclosure
-//			// - convention
-//			// - escaping
-//			// - throws
-//			// - rethrows
-//
-//			if string.contains("autoclosure") ||
-//				string.contains("convention") ||
-//				string.contains("escaping") ||
-//				string.contains("throws")
-//			{
-//				print("Error parsing:")
-//				print(string)
-//				fatalError(string)
-//			}
-//
-//			// Two special cases: types with `inout` and `__owned` prefixes
-//			if string[index...].hasPrefix("inout ") {
-//				index = string.index(index, offsetBy: "inout ".count)
-//				return parseNonOptionalType()
-//			}
-//
-//			if string[index...].hasPrefix("__owned ") {
-//				index = string.index(index, offsetBy: "__owned ".count)
-//				return parseNonOptionalType()
-//			}
-//
-//			// Arrays, dictionarys and tuples/functions can start with brackets and parentheses, so
-//			// we can detect them earlier. Generics and optionals can only be detected later since
-//			// the "<...>" and "?" are postfix.
-//
-//			// If it's an array or a dictionary
-//			if string[index] == "[" {
-//				index = string.index(after: index)
-//
-//				guard let subType1 = parseType() else {
-//					return nil
-//				}
-//
-//				// If it's an array
-//				if string[index] != ":" {
-//					guard string[index] == "]" else {
-//						return nil
-//					}
-//
-//					index = string.index(after: index)
-//					return .array(of: subType1)
-//				}
-//				else {
-//					// if it's a dictionary
-//
-//					index = string.index(after: index)
-//
-//					guard let subType2 = parseType() else {
-//						return nil
-//					}
-//
-//					guard string[index] == "]" else {
-//						return nil
-//					}
-//
-//					index = string.index(after: index)
-//					return .generic(typeName: "Dictionary", genericArguments: [subType1, subType2])
-//				}
-//			}
-//
-//			// If it's a tuple
-//			if string[index] == "(" {
-//				index = string.index(after: index)
-//
-//				// Check for labels before the tuple type, i.e. `(bla: Int, foo: String)` should be
-//				// parsed as `(Int, String)`
-//
-//				let subType1: SwiftType
-//				guard let firstAttempt = parseType() else {
-//					return nil
-//				}
-//				// If we read a label instead of a type, try again
-//				if string[index] == ":" {
-//					index = string.index(after: index)
-//					cleanLeadingWhitespace()
-//					guard let secondAttempt = parseType() else {
-//						return nil
-//					}
-//					subType1 = secondAttempt
-//				}
-//				else {
-//					subType1 = firstAttempt
-//				}
-//
-//				let tupleElements: MutableList = [subType1]
-//
-//				while string[index] == "," {
-//					index = string.index(after: index)
-//
-//					// Check for labels just as before
-//					let newSubType: SwiftType
-//					guard let firstAttempt = parseType() else {
-//						return nil
-//					}
-//					if string[index] == ":" {
-//						index = string.index(after: index)
-//						cleanLeadingWhitespace()
-//						guard let secondAttempt = parseType() else {
-//							return nil
-//						}
-//						newSubType = secondAttempt
-//					}
-//					else {
-//						newSubType = firstAttempt
-//					}
-//
-//					tupleElements.append(newSubType)
-//				}
-//
-//				guard string[index] == ")" else {
-//					return nil
-//				}
-//				index = string.index(after: index)
-//				cleanLeadingWhitespace()
-//
-//				// Check if it's a standard tuple or if it's part of a function type
-//
-//				if string[index...].hasPrefix("->") {
-//					// If it's a function type, skip the "->" and any possible whitespace after it
-//					index = string.index(index, offsetBy: 2)
-//					cleanLeadingWhitespace()
-//
-//					guard let returnType = parseType() else {
-//						return nil
-//					}
-//
-//					return .function(parameters: tupleElements, returnType: returnType)
-//				}
-//				else {
-//					// If it's not a function it can still be a simple wrapping parameter
-//					// i.e. ((String) -> String)?
-//					if tupleElements.count == 1 {
-//						return tupleElements[0]
-//					}
-//					else {
-//						return .tuple(subTypes: tupleElements.map {
-//							LabeledSwiftType(label: nil, swiftType: $0)
-//						}.toMutableList())
-//					}
-//				}
-//			}
-//
-//			// If it doesn't start with a special character, try to read it as a normal type
-//			var normalTypeEndIndex = index
-//			while normalTypeEndIndex != string.endIndex,
-//				(string[normalTypeEndIndex].isLetter ||
-//					string[normalTypeEndIndex].isNumber ||
-//					string[normalTypeEndIndex] == "_")
-//			{
-//				normalTypeEndIndex = string.index(after: normalTypeEndIndex)
-//			}
-//
-//			// If it's empty, something went wrong
-//			guard normalTypeEndIndex != index else {
-//				return nil
-//			}
-//
-//			let normalType = String(string[index..<normalTypeEndIndex])
-//			index = normalTypeEndIndex
-//			cleanLeadingWhitespace()
-//
-//			// Check if it's a generic
-//			if index != string.endIndex, string[index] == "<" {
-//				index = string.index(after: index)
-//
-//				guard let subType1 = parseType() else {
-//					return nil
-//				}
-//
-//				let genericElements: MutableList = [subType1]
-//
-//				while string[index] == "," {
-//					index = string.index(after: index)
-//
-//					guard let newSubType = parseType() else {
-//						return nil
-//					}
-//
-//					genericElements.append(newSubType)
-//				}
-//
-//				guard string[index] == ">" else {
-//					return nil
-//				}
-//				index = string.index(after: index)
-//
-//				// If it's an optional written as a generic
-//				if normalType == "Optional", genericElements.count == 1 {
-//					return .optional(subType: genericElements[0])
-//				}
-//
-//				// Otherwise, it's just a normal generic type
-//				return .generic(typeName: normalType, genericArguments: genericElements)
-//			}
-//
-//			// Otherwise, consider it a normal type
-//			return .namedType(typeName: normalType)
-//		}
-//	}
-//
-//	func isSubtype(of superType: SwiftType) -> Bool {
-//		// Trivial case
-//		if self == superType {
-//			return true
-//		}
-//
-//		// Special cases that are considered superTypes of anything (for simplicity in the standard
-//		// library translations)
-//		if case let .namedType(typeName: namedSuperType) = superType {
-//			if namedSuperType == "Any" ||
-//				namedSuperType == "_Any" ||
-//				namedSuperType == "_Hashable" ||
-//				namedSuperType == "_Comparable" ||
-//				namedSuperType == "_Optional"
-//			{
-//				return true
-//			}
-//		}
-//		if case let .optional(subType: optionalSuperType) = superType {
-//			if case let .namedType(typeName: namedSuperType) = optionalSuperType {
-//				if namedSuperType == "_Optional" {
-//					if case .optional = self {
-//						return true
-//					}
-//					else {
-//						return false
-//					}
-//				}
-//			}
-//		}
-//
-//		// Tuples are subtypes if their components are subtypes
-//		if case let .tuple(subTypes: selfSubTypes) = self,
-//			case let .tuple(subTypes: superSubTypes) = superType
-//		{
-//			guard selfSubTypes.count == superSubTypes.count else {
-//				return false
-//			}
-//
-//			for (selfSubType, superSubType) in zip(selfSubTypes, selfSubTypes) {
-//				guard selfSubType.swiftType.isSubtype(of: superSubType.swiftType) else {
-//					return false
-//				}
-//			}
-//		}
-//
-//		// Try to simplify the types, if possible
-//		if let simpleSelf = simplifyType(self) {
-//			return simpleSelf.isSubtype(of: superType)
-//		}
-//		else if let simpleSuperType = simplifyType(superType) {
-//			return self.isSubtype(of: simpleSuperType)
-//		}
-//
-//		// Handle optionals:
-//		// X? < Y? <=> X < Y
-//		// X < Y? <=> X < Y
-//		if case let .optional(subType: optionalSuperType) = superType {
-//			if case let .optional(subType: optionalSelf) = self {
-//				return optionalSelf.isSubtype(of: optionalSuperType)
-//			}
-//			else {
-//				return self.isSubtype(of: optionalSuperType)
-//			}
-//		}
-//
-//		// Handle functions:
-//		// Functions are always considered a subType of one another. They have edge cases that are
-//		// difficult to handle correctly, and it's rare for them to appear so false positives should
-//		// also be rare.
-//		if case .function = superType {
-//			if case .function = self {
-//				return true
-//			}
-//			else {
-//				return false
-//			}
-//		}
-//
-//		// Handle arrays
-//		if let superElementType = superType.arrayElement {
-//			if let selfElementType = self.arrayElement {
-//				return selfElementType.isSubtype(of: superElementType)
-//			}
-//			else {
-//				return false
-//			}
-//		}
-//
-//		// Handle dictionaries
-//		if let (superKey, superValue) = superType.dictionaryKeyAndValue {
-//			if let (selfKey, selfValue) = self.dictionaryKeyAndValue {
-//				return selfKey.isSubtype(of: superKey) && selfValue.isSubtype(of: superValue)
-//			}
-//			else {
-//				return false
-//			}
-//		}
-//
-//		// Handle generics
-//		if case let .generic(
-//			typeName: superTypeName,
-//			genericArguments: superTypeArguments) = superType
-//		{
-//			if case let .generic(
-//				typeName: selfTypeName,
-//				genericArguments: selfTypeArguments) = self
-//			{
-//				// Check if the named parts are the same
-//				let namedSuperType = SwiftType.namedType(typeName: superTypeName)
-//				let namedSelfType = SwiftType.namedType(typeName: selfTypeName)
-//				guard namedSelfType.isSubtype(of: namedSuperType) else {
-//					return false
-//				}
-//
-//				// Check if the arguments are the same
-//				guard superTypeArguments.count == selfTypeArguments.count else {
-//					return false
-//				}
-//
-//				for (selfTypeArgument, superTypeArgument) in
-//					zip(selfTypeArguments, superTypeArguments)
-//				{
-//					guard selfTypeArgument.isSubtype(of: superTypeArgument) else {
-//						return false
-//					}
-//				}
-//			}
-//			else {
-//				return false
-//			}
-//		}
-//
-//		return false
-//	}
-//
-//	private func simplifyType(_ gryphonType: SwiftType) -> SwiftType? {
-//		// Deal with standard library types that can be handled as other types
-//		if case let .namedType(typeName: typeName) = gryphonType {
-//			if let result = Utilities.getTypeMapping(for: typeName) {
-//				return .namedType(typeName: result)
-//			}
-//		}
-//
-//		if case let .generic(typeName: typeName, genericArguments: genericArguments) = gryphonType {
-//			if gryphonType.arrayElement != nil {
-//				return gryphonType
-//			}
-//
-//			// Treat Slice as Array
-//			if typeName == "Slice",
-//				genericArguments.count == 1,
-//				case let .generic(
-//					typeName: innerTypeName,
-//					genericArguments: innerGenericArguments) = genericArguments[0]
-//			{
-//				if innerTypeName == "MutableList" || innerTypeName == "List" {
-//					return .generic(
-//						typeName: innerTypeName,
-//						genericArguments: [innerGenericArguments[0]])
-//				}
-//			}
-//
-//			// Treat MutableMap as Dictionary
-//			if typeName == "MutableMap" || typeName == "Map" {
-//				// MutableMap should have exactly two generic argument: a key and a value
-//				return .dictionary(withKey: genericArguments[0], value: genericArguments[1])
-//			}
-//		}
-//
-//		return nil
-//	}
