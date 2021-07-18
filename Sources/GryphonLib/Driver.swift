097c87c32ae53073b29b91d7e2065d65e5ca9077
Vini Vendramini <vinivendra@me.com>
2021-07-18 15:03:26 -0300

    Meta: version bump

--- Sources/GryphonLib/Driver.swift
+++ Sources/GryphonLib/Driver.swift
@@ -17,7 +17,7 @@
 //
 
 public class Driver {
-	public static let gryphonVersion = "0.16"
+	public static let gryphonVersion = "0.16.1"
 
 	public static let supportedArguments: List = [
 		"help", "-help", "--help",
